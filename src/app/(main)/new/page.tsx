4376e5063cb7424844c8e37dd22c4c470a1af496
DiceR011 <touhu0305@gmail.com>
2024-07-16 14:13:25 +0900

    NextでのTask管理アプリ

---
+++ src/app/(main)/new/page.tsx
@@ -0,0 +1,12 @@
+import NewTaskFrom from "@/app/components/NewTaskForm/NewTaskFrom"
+
+const NewTaskPage = () => {
+    return (
+        <div className="flex flex-col justify-center py-20">
+            <h2 className="text-center text-2xl font-bold">Create New Task</h2>
+            <NewTaskFrom />
+        </div>
+    )
+}
+
+export default NewTaskPage
